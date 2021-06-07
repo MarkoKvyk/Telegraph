a63b25aefde11393341700d20a62221907bb672d
Marko Kvyk <kvykmarko@gmail.com>
2021-06-07 19:27:40 +0300

    code cleanup

--- Telegraph/Telegraph.Test/Constants.cs
+++ Telegraph/Telegraph.Test/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Telegraph.Test
+﻿namespace Telegraph.Test
 {
     public static class Constants
     {
--- Telegraph/Telegraph/TelegraphClient.cs
+++ Telegraph/Telegraph/TelegraphClient.cs
@@ -4,7 +4,6 @@ using Kvyk.Telegraph.Models.Requests;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
